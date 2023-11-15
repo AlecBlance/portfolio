0c956400157c91decab3e873ab9804e11f34ffc1
Alec Blance <17378596+AlecBlance@users.noreply.github.com>
2023-11-15 08:05:28 +0800

    fix: missing links

--- components/Socials.tsx
+++ components/Socials.tsx
@@ -3,16 +3,16 @@ import { FacebookIcon, GithubIcon, GmailIcon, LinkedinIcon } from "./Icon";
 const Socials = () => {
   return (
     <div className="flex items-center gap-x-3 justify-around">
-      <a href="/">
+      <a href="https://github.com/alecblance">
         <GithubIcon className="w-6 h-6 mx-1" />
       </a>
-      <a href="/">
+      <a href="mailto:blancealec1@gmail.com">
         <GmailIcon className="w-6 h-6 mx-1" />
       </a>
-      <a href="/">
+      <a href="https://www.linkedin.com/in/alecblance/">
         <LinkedinIcon className="w-6 h-6 mx-1" />
       </a>
-      <a href="/">
+      <a href="https://facebook.com/alec.blance">
         <FacebookIcon className="w-6 h-6 mx-1" />
       </a>
     </div>
