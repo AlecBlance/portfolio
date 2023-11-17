a8b13b2d31914804480a600ba595175cb2f0243f
Alec Blance <17378596+AlecBlance@users.noreply.github.com>
2023-11-17 15:11:50 +0800

    style: fix inconsistent sizing

--- components/Tab/Projects.tsx
+++ components/Tab/Projects.tsx
@@ -40,7 +40,7 @@ const Project = ({ project }: { project: Project }) => {
           {project.liveSite && (
             <a
               href={project.liveSite}
-              className="mr-3 bg-portfolio-accent text-portfolio-secondary py-2 px-5 rounded-lg text-sm"
+              className="mr-3 border border-portfolio-accent bg-portfolio-accent text-portfolio-secondary py-2 px-5 rounded-lg text-sm"
             >
               Live site
             </a>
