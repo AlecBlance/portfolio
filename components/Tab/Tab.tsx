67a7b7c25a120365cbd995d2516110b4b38efee3
Alec Blance <17378596+AlecBlance@users.noreply.github.com>
2023-11-17 08:02:02 +0800

    fix: mouse lag

--- components/Tab/Tab.tsx
+++ components/Tab/Tab.tsx
@@ -12,8 +12,11 @@ const Tab = () => {
   useEffect(() => {
     const movingCircle = document.querySelector<HTMLElement>(".moving-circle");
     document.addEventListener("mousemove", function (e) {
-      movingCircle!.style.top = `${e.clientY - 20}px`;
-      movingCircle!.style.left = `${e.clientX - 20}px`;
+      window.requestAnimationFrame(function () {
+        movingCircle!.style.transform = `translateX(${
+          e.clientX - 20
+        }px) translateY(${e.clientY + 20}px)`;
+      });
     });
   }, []);
 
