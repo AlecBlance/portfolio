a6a29367592e7a90b84a9e712019cb3366303d28
Alec Blance <17378596+AlecBlance@users.noreply.github.com>
2023-11-29 16:20:37 +0800

    refactor: 🎭 skills section

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,7 @@ import {
   FigmaIcon,
   GitIcon,
   ExpressIcon,
+  NextIcon,
 } from "@/components/Icon";
 import Visuals from "@/components/Tab/Visuals";
 import Socials from "@/components/Socials";
@@ -17,30 +18,30 @@ import Socials from "@/components/Socials";
 const Home = () => {
   return (
     <>
-      <div className="relative min-h-[100dvh] flex flex-col">
-        <div className="pointer-events-none fixed left-0 top-[-2.5rem] z-[99] h-10 w-10 rounded-full bg-indigo-400 mix-blend-screen duration-75 ease-in-out moving-circle max-lg:hidden"></div>
-        {/* <div className="flex justify-center items-center absolute w-full h-full z-[-1]">
-          <div className="h-[100vh] max-h-[100vw] w-[100vh] rounded-full justify-self-center bg-portfolio-accent light blur-[35vw] md:blur-[25vh]"></div>
+      <div className="relative flex min-h-[100dvh] flex-col">
+        <div className="moving-circle pointer-events-none fixed left-0 top-[-2.5rem] z-[99] h-10 w-10 rounded-full bg-indigo-400 mix-blend-screen duration-75 ease-in-out max-lg:hidden"></div>
+        {/* <div className="absolute z-[-1] flex h-full w-full items-center justify-center">
+          <div className="light h-[100vh] max-h-[100vw] w-[100vh] justify-self-center rounded-full bg-portfolio-accent blur-[35vw] md:blur-[25vh]"></div>
         </div> */}
 
         <div
-          className="flex justify-between sm:mx-[15vw] mt-6 px-12 sm:px-0 max-xs:px-[5vw]"
+          className="mt-6 flex justify-between px-12 max-xs:px-[5vw] sm:mx-[15vw] sm:px-0"
           data-aos="fade-zoom-in"
           data-aos-easing="ease-in-out"
           data-aos-duration="800"
           data-aos-delay="1000"
         >
-          <div className="flex items-center py-1 px-4 rounded-md border border-portfolio-accent hover:bg-portfolio-accent font-light ease-in-out transition-all max-xs:text-xs max-xs:py-2">
+          <div className="flex items-center rounded-md border border-portfolio-accent px-4 py-1 font-light transition-all ease-in-out hover:bg-portfolio-accent max-xs:py-2 max-xs:text-xs">
             <a href="https://drive.google.com/file/d/182cqMIL5egzjUxaYCUp5-RQaMRXGsUjQ/view?usp=sharing">
               My Resume
             </a>
           </div>
           <Socials />
         </div>
-        <div className="h-full w-full flex-grow flex items-center">
-          <div className="h-[45%] flex flex-col w-full items-center gap-y-10">
+        <div className="flex h-full w-full flex-grow items-center">
+          <div className="flex h-[45%] w-full flex-col items-center gap-y-10">
             <Image
-              className="rounded-full w-32 lg:w-36 xl:w-40"
+              className="w-32 rounded-full lg:w-36 xl:w-40"
               src="/profile.jpg"
               alt="Alec Blance Profile Image"
               width={150}
@@ -50,9 +51,9 @@ const Home = () => {
               data-aos-duration="500"
               data-aos-delay="100"
             />
-            <div className="flex flex-col gap-y-5 xl:gap-y-8 items-center">
+            <div className="flex flex-col items-center gap-y-5 xl:gap-y-8">
               <p
-                className="font-extralight text-xl lg:text-2xl xl:text-3xl"
+                className="text-xl font-extralight lg:text-2xl xl:text-3xl"
                 data-aos="fade-zoom-in"
                 data-aos-easing="ease"
                 data-aos-duration="500"
@@ -61,7 +62,7 @@ const Home = () => {
                 Hi, 👋🏻 I am{" "}
               </p>
               <h1
-                className="text-5xl font-semibold gradient lg:text-6xl xl:text-7xl"
+                className="gradient text-5xl font-semibold lg:text-6xl xl:text-7xl"
                 data-aos="fade-zoom-in"
                 data-aos-easing="ease"
                 data-aos-duration="500"
@@ -70,7 +71,7 @@ const Home = () => {
                 Alec Blance
               </h1>
               <p
-                className="font-extralight text-xl lg:text-2xl xl:text-3xl"
+                className="text-xl font-extralight lg:text-2xl xl:text-3xl"
                 data-aos="fade-zoom-in"
                 data-aos-easing="ease"
                 data-aos-duration="500"
@@ -87,7 +88,7 @@ const Home = () => {
         <Projects />
         <Visuals />
       </div>
-      <div className="py-6 px-12 mt-12 md:py-12 bg-portfolio-black bg-opacity-40 sm:px-[15vw] border-y border-portfolio-glass-stroke/40">
+      <div className="mt-12 border-y border-portfolio-glass-stroke/40 bg-portfolio-black bg-opacity-40 px-12 py-6 sm:px-[15vw] md:py-12">
         <h1
           className="mb-3"
           data-aos="fade-zoom-in"
@@ -95,12 +96,12 @@ const Home = () => {
           data-aos-duration="500"
           data-aos-delay="100"
         >
-          <span className="font-bold max-xs:text-lg gradient text-2xl lg:text-3xl">
+          <span className="gradient text-2xl font-bold max-xs:text-lg lg:text-3xl">
             Skills
           </span>
         </h1>
         <p
-          className="lg:text-xl max-xs:text-sm"
+          className="max-xs:text-sm lg:text-xl"
           data-aos="fade-zoom-in"
           data-aos-easing="ease"
           data-aos-duration="500"
@@ -108,7 +109,7 @@ const Home = () => {
         >
           I am comfortable working with these technologies.
         </p>
-        <div className="mt-6 md:mt-12 w-full grid skills-grid gap-4 lg:text-lg max-xs:gap-2">
+        <div className="skills-grid mt-6 grid w-full gap-4 max-xs:gap-2 md:mt-12 lg:text-lg">
           <div
             className="flex items-center first:ml-0"
             data-aos="fade-zoom-in"
@@ -130,7 +131,7 @@ const Home = () => {
             <p className="max-xs:text-sm">Typescript</p>
           </div>
           <div
-            className="flex items-center  first:ml-0"
+            className="flex items-center first:ml-0"
             data-aos="fade-zoom-in"
             data-aos-easing="ease"
             data-aos-duration="500"
@@ -140,7 +141,7 @@ const Home = () => {
             <p className="max-xs:text-sm">Tailwind</p>
           </div>
           <div
-            className="flex items-center  first:ml-0"
+            className="flex items-center first:ml-0"
             data-aos="fade-zoom-in"
             data-aos-easing="ease"
             data-aos-duration="500"
@@ -150,7 +151,7 @@ const Home = () => {
             <p className="max-xs:text-sm">Node</p>
           </div>
           <div
-            className="flex items-center  first:ml-0"
+            className="flex items-center first:ml-0"
             data-aos="fade-zoom-in"
             data-aos-easing="ease"
             data-aos-duration="500"
@@ -160,7 +161,7 @@ const Home = () => {
             <p className="max-xs:text-sm">Express</p>
           </div>
           <div
-            className="flex items-center  first:ml-0"
+            className="flex items-center first:ml-0"
             data-aos="fade-zoom-in"
             data-aos-easing="ease"
             data-aos-duration="500"
@@ -170,7 +171,7 @@ const Home = () => {
             <p className="max-xs:text-sm">MongoDB</p>
           </div>
           <div
-            className="flex items-center  first:ml-0"
+            className="flex items-center first:ml-0"
             data-aos="fade-zoom-in"
             data-aos-easing="ease"
             data-aos-duration="500"
@@ -180,7 +181,7 @@ const Home = () => {
             <p className="max-xs:text-sm">Figma</p>
           </div>
           <div
-            className="flex items-center  first:ml-0"
+            className="flex items-center first:ml-0"
             data-aos="fade-zoom-in"
             data-aos-easing="ease"
             data-aos-duration="500"
@@ -189,12 +190,22 @@ const Home = () => {
             <GitIcon className="mr-2 h-8 w-8 max-xs:w-5" />
             <p className="max-xs:text-sm">Git</p>
           </div>
+          <div
+            className="flex items-center first:ml-0"
+            data-aos="fade-zoom-in"
+            data-aos-easing="ease"
+            data-aos-duration="500"
+            data-aos-delay="1200"
+          >
+            <NextIcon className="mr-2 h-6 w-6 max-xs:w-5" />
+            <p className="max-xs:text-sm">NextJS</p>
+          </div>
         </div>
       </div>
-      <div className="min-h-[70vh] max-xs:min-h-[90vh] h-[40vw] flex flex-col relative overflow-hidden">
-        <div className="flex-grow mx-12 flex flex-col items-center justify-center">
+      <div className="relative flex h-[40vw] min-h-[70vh] flex-col overflow-hidden max-xs:min-h-[90vh]">
+        <div className="mx-12 flex flex-grow flex-col items-center justify-center">
           <h1
-            className="gradient text-5xl text-center leading-tight lg:text-6xl lg:leading-normal xl:text-7xl xl:leading-relaxed"
+            className="gradient text-center text-5xl leading-tight lg:text-6xl lg:leading-normal xl:text-7xl xl:leading-relaxed"
             data-aos="fade-zoom-in"
             data-aos-easing="ease"
             data-aos-duration="500"
@@ -204,7 +215,7 @@ const Home = () => {
           </h1>
           <a
             href="mailto:blancealec1@gmail.com"
-            className="bg-portfolio-glass/30 mt-12 max-xs:text-lg border border-portfolio-secondary border-opacity-20 px-12 py-5 text-2xl rounded-lg"
+            className="mt-12 rounded-lg border border-portfolio-secondary border-opacity-20 bg-portfolio-glass/30 px-12 py-5 text-2xl max-xs:text-lg"
             data-aos="fade-up"
             data-aos-easing="ease"
             data-aos-duration="800"
@@ -213,17 +224,17 @@ const Home = () => {
             Say Hi
           </a>
         </div>
-        <div className="flex justify-between bg-portfolio-glass bg-opacity-10 border-b border-b-portfolio-glass-stroke p-3 sm:px-[15vw]">
+        <div className="flex justify-between border-b border-b-portfolio-glass-stroke bg-portfolio-glass bg-opacity-10 p-3 sm:px-[15vw]">
           <p className="gradient max-xs:text-xs">Alec Blance © 2023</p>
           <div className="relative">
-            <div className="animate-bounce absolute bg-portfolio-accent top-[-3.5rem] px-2 py-1 left-[-9rem] rounded-lg rounded-br-none">
+            <div className="absolute left-[-9rem] top-[-3.5rem] animate-bounce rounded-lg rounded-br-none bg-portfolio-accent px-2 py-1">
               <p className="text-xs">More projects on my Github!</p>
             </div>
             <Socials />
           </div>
         </div>
-        {/* <div className="flex absolute w-full h-full z-[-1] items-end bottom-[-50vw] md:bottom-[-70vh] justify-center ">
-          <div className="h-[100vh] max-h-[100vw] w-[100vh] rounded-full bg-portfolio-accent light blur-[35vw] md:blur-[25vh]"></div>
+        {/* <div className="absolute bottom-[-50vw] z-[-1] flex h-full w-full items-end justify-center md:bottom-[-70vh]">
+          <div className="light h-[100vh] max-h-[100vw] w-[100vh] rounded-full bg-portfolio-accent blur-[35vw] md:blur-[25vh]"></div>
         </div> */}
       </div>
     </>
